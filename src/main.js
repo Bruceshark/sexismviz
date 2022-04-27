b971d86546dc875cc36accaf6a39f47a6e18583d
Bruceshark <yyubv@connect.ust.hk>
2022-04-27 19:47:42 +0800

    set the global style for antd

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import { Table, Modal } from 'ant-design-vue';
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.less'
 
 const app = createApp(App);
 
-app.use(Table, Modal).mount('#app');
+app.use().mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +17,7 @@ export default defineConfig({
     preprocessorOptions: {
        less: {
          modifyVars: {
-           hack: `true; @import (reference) "${path.resolve(__dirname, 'src/css/global.less')}";`, 
+           'primary-color': '#c92268' 
          },
          javascriptEnabled: true
        }
